cb1f740af0794b0e767c4f6f750e18a46ae26bc8
闫 强 <yanhaiqiang1234@163.com>
2017-07-18 14:02:55 +0800

    cell添加webView高度自适应
    使用WKWebView自适应高度

---
+++ HQWebView/HQWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQWebView
+//
+//  Created by admin on 2017/7/18.
+//  Copyright © 2017年 judian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQWebView/HQWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQWebView
+//
+//  Created by admin on 2017/7/18.
+//  Copyright © 2017年 judian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
